e3f646f0dbe0d91249818af40ae9048f3fe0dafa
sangukO <woqls1972@gamil.com>
2021-03-24 11:07:30 +0900

    0324

--- hello_client.c
+++ hello_client.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 
 void error_handling(char *message);
@@ -10,13 +11,12 @@ void error_handling(char *message);
 int main(int argc, char *argv[])
 {
 	int sock;
-	
 	struct sockaddr_in serv_addr;
 	char message[30];
-	int str_len;
+	int str_len=0;
+	int idx=0, read_len=0;
 
-	if(argc!=3)
-	{
+	if(argc!=3){
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
 	sock = socket(PF_INET, SOCK_STREAM, 0);
 	if(sock == -1)
 		error_handling("socket() error");
+	
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family=AF_INET;
 	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
@@ -31,10 +32,17 @@ int main(int argc, char *argv[])
 
 	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
 		error_handling("socket() error");
-	str_len = read(sock,message,sizeof(message)-1);
-	if(str_len==-1)
-		error_handling("socket() error!");
+
+	while(read_len=read(sock, &message[idx++], 1))
+	{
+		if(read_len==-1)
+			error_handling("read() error!");
+
+		str_len+=read_len;
+	}
+	
 	printf("Message from server : %s \n", message);
+	printf("Function read call count : %d \n", str_len);
 	close(sock);
 	return 0;
 }
